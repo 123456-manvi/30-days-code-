1272650c190baf965a10be6d397adb32c711a967
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2023-04-12 23:35:35 +0530

    Create print nth row of pascal triangle.cpp

---
+++ print nth row of pascal triangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int ans=1;
+  print(ans);
+  for(i=1;i<n;i++)
+  {
+    ans=ans*(n-i);
+    ans=ans/i;
+    print(ans);
+  }
+  
+ TC= O(N)
+   SC=O(1)
